7f34aa670322ef4a6cc11366e2f262402ae14d0f
OrangeUtan <Oran9eUtan@gmail.com>
2015-11-12 16:54:57 +0100

    Dokumentation hinzugefügt und neue Methoden hinzugefügt

--- app/src/main/java/com/github/wksb/wkebapp/activity/navigation/Route.java
+++ app/src/main/java/com/github/wksb/wkebapp/activity/navigation/Route.java
@@ -4,7 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Michael on 27.10.2015.
+ * This class manages a collection of {@link RouteSegment}s
+ *
+ * @author Projekt-Seminar "Schnitzeljagd World-heritage" 2015/2016 des Clavius Gymnasiums Bamberg
+ * @version 1.0
+ * @since 2015-06-04
  */
 public class Route {
     private final String name;
@@ -15,11 +19,36 @@ public class Route {
         this.routeSegmentsList = new ArrayList<>();
     }
 
+    /**
+     * Add a {@link RouteSegment} to this Route
+     * @param routeSegment The {@link RouteSegment} to be added
+     */
     public void addRouteSegment(RouteSegment routeSegment) {
         routeSegmentsList.add(routeSegment);
     }
 
+    /**
+     * Get a List of the {@link RouteSegment}s of this Route
+     * @return A List containing all {@link RouteSegment}s of this Route
+     */
     public List<RouteSegment> getRouteSegments() {
         return this.routeSegmentsList;
+    }
+
+    /**
+     * Get the {@link RouteSegment} at a specific position in the List
+     * @param position The position of the {@link RouteSegment}
+     * @return The {@link RouteSegment} at the specific position
+     */
+    public RouteSegment getRouteSegmentAt(int position) {
+        return this.routeSegmentsList.get(position);
+    }
+
+    /**
+     * Get the the number of {@link RouteSegment} in this Route
+     * @return The number of {@link RouteSegment} in this Route
+     */
+    public int getRouteSegmentAmount() {
+        return this.routeSegmentsList.size();
     }
 }
