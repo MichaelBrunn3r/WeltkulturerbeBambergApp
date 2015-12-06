94799f37aefdc4ed0a078ae28a83de34e0342208
OrangeUtan <Oran9eUtan@gmail.com>
2015-12-06 18:41:13 +0100

    Beim empfangen eines Proximity Alerts wid jetzt ein Quiz gesartet

--- app/src/main/java/com/github/wksb/wkebapp/ProximityAlertReceiver.java
+++ app/src/main/java/com/github/wksb/wkebapp/ProximityAlertReceiver.java
@@ -3,8 +3,11 @@ package com.github.wksb.wkebapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.location.LocationListener;
 import android.location.LocationManager;
 
+import com.github.wksb.wkebapp.activity.QuizActivity;
+import com.github.wksb.wkebapp.activity.navigation.Route;
 import com.github.wksb.wkebapp.utilities.DebugUtils;
 
 //TODO Documentation
@@ -18,7 +21,14 @@ public class ProximityAlertReceiver extends BroadcastReceiver{
         boolean entering = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false);
 
         if (entering) {
-            DebugUtils.toast(context, "Im entering " + intent.getStringExtra("name") + " (Lat: " + intent.getFloatExtra("lat", 0.0f) + ", Lng: " + intent.getFloatExtra("lng", 0.0f) + ")");
+            DebugUtils.toast(context, "Im entering " + intent.getStringExtra("name"));
+            if (intent.getIntExtra("quiz-id", -1) == Route.getCurrentQuizId(context)) {
+                QuizActivity.setProgressState(context, QuizActivity.IS_IN_PROGRESS);
+
+                Intent startQuizActivity = new Intent(context, QuizActivity.class);
+                startQuizActivity.putExtra(QuizActivity.TAG_QUIZ_ID, Route.getCurrentQuizId(context));
+                context.startActivity(startQuizActivity);
+            }
         } else {
             DebugUtils.toast(context, "Im doing something funny :D");
         }
