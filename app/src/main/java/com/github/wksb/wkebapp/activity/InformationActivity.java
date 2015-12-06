a476c2d08c614f027249bf17f28d0cc60f91000b
OrangeUtan <Oran9eUtan@gmail.com>
2015-12-06 16:39:43 +0100

    Wenn man jetzt in der InformationActity den "Back" Button drückt, wird man in deren ParentActity navigiert

--- app/src/main/java/com/github/wksb/wkebapp/activity/InformationActivity.java
+++ app/src/main/java/com/github/wksb/wkebapp/activity/InformationActivity.java
@@ -2,6 +2,7 @@ package com.github.wksb.wkebapp.activity;
 
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -55,6 +56,11 @@ public class InformationActivity extends AppCompatActivity {
 
         // Set up the Information
         setUpInformation();
+    }
+
+    @Override
+    public void onBackPressed() {
+        NavUtils.navigateUpTo(this, getParentActivityIntent()); // Navigate to Parent Activity
     }
 
     private void setUpActionBar() {
