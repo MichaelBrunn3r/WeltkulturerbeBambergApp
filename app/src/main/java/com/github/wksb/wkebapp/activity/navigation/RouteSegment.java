2534b139878f07111a15fcb0e381e191e4e525b8
OrangeUtan <Oran9eUtan@gmail.com>
2015-12-06 06:43:21 +0100

    Wegpunkte werden jetzt zentral aus der Route bezogen. Die Polyline wird direkt gespeichert und nicht mehr nur der Filename gespeichert

--- app/src/main/java/com/github/wksb/wkebapp/activity/navigation/RouteSegment.java
+++ app/src/main/java/com/github/wksb/wkebapp/activity/navigation/RouteSegment.java
@@ -2,69 +2,68 @@ package com.github.wksb.wkebapp.activity.navigation;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
 
-import com.github.wksb.wkebapp.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by Michael on 27.10.2015.
  */
 public class RouteSegment {
 
+    private Route route;
     private int fromWaypointID;
     private int toWaypointID;
-    private Waypoint fromWaypoint;
-    private Waypoint toWaypoint;
-    private String filename;
+    private PolylineOptions polyline;
 
-    public RouteSegment(int fromWaypointID, int toWaypointID, String filename) {
+    public RouteSegment(Route route, int fromWaypointID, int toWaypointID, PolylineOptions polyline) {
+        this.route = route;
         this.fromWaypointID = fromWaypointID;
         this.toWaypointID = toWaypointID;
-        this.filename = filename;
+        this.polyline = polyline;
+    }
+
+    public Context getContext() {
+        return route.getContext();
+    }
+
+    public int[] getWaypointIds() {
+        return new int[]{fromWaypointID, toWaypointID};
+    }
+
+    public void renderOnMap(GoogleMap googleMap) {
+
+        // Add the Marker for the from and to Waypoint to the map
+        addMarkersToMap(googleMap);
+
+        // Add the Polyline showing the Route to the Map
+        addPolylineToMap(googleMap);
     }
 
     /**
      * Initialises this RouteSegment. Adds Markers for the Start and Destination, the Polyline connecting the two Markers and a Proximity for the Destination
-     * @param map The Map on which to add the Markers and the Polyline
+     * @param googleMap The Map on which to add the Markers and the Polyline
      */
-    public void init(Activity activity, GoogleMap map) {
-        // Get the from and to Waypoints, if they don't already exist
-        if (fromWaypoint == null) fromWaypoint = new Waypoint(activity, fromWaypointID);
-        if (toWaypoint == null) toWaypoint = new Waypoint(activity, toWaypointID);
+    public void init(GoogleMap googleMap) {
 
         // Set the current Quiz to the Quiz of the current destination Waypoint
-        Route.setCurrentQuizId(activity, toWaypoint.getQuizID());
-
-        // Clear the Map from all Markers, polylines, overlays, etc.
-        map.clear();
-
-        // Add the Marker for the from and to Waypoint to the map
-        addMarkersToMap(map);
-
-        // Add the Polyline showing the Route to the Map
-        addPolylineToMap(activity, map);
+        Route.setCurrentQuizId(getContext(), route.getWaypointById(toWaypointID).getQuizId());
 
         // Add the proximity alert at the destination Waypoint
-        addProximityAlert(activity);
+        addProximityAlert();
 
         // Zoom to the Route Segment on the Map
-        map.animateCamera(CameraUpdateFactory
-                .newLatLngZoom(new LatLng((fromWaypoint.getLatitude() + toWaypoint.getLatitude()) / 2, (fromWaypoint.getLongitude() + toWaypoint.getLongitude()) / 2), 14));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                        new LatLng(
+                            (route.getWaypointById(fromWaypointID).getLatitude() + route.getWaypointById(toWaypointID).getLatitude()) / 2,
+                            (route.getWaypointById(fromWaypointID).getLongitude() + route.getWaypointById(toWaypointID).getLongitude()) / 2)
+                        , 14));
     }
 
     /**
@@ -73,11 +72,11 @@ public class RouteSegment {
      */
     private void addMarkersToMap(GoogleMap map) {
         MarkerOptions fromWapointMarker = new MarkerOptions()
-                .title(fromWaypoint.getName())
-                .position(new LatLng(fromWaypoint.getLatitude(), fromWaypoint.getLongitude()));
+                .title(route.getWaypointById(fromWaypointID).getName())
+                .position(new LatLng(route.getWaypointById(fromWaypointID).getLatitude(), route.getWaypointById(fromWaypointID).getLongitude()));
         MarkerOptions toWapointMarker = new MarkerOptions()
-                .title(toWaypoint.getName())
-                .position(new LatLng(toWaypoint.getLatitude(), toWaypoint.getLongitude()));
+                .title(route.getWaypointById(toWaypointID).getName())
+                .position(new LatLng(route.getWaypointById(toWaypointID).getLatitude(), route.getWaypointById(toWaypointID).getLongitude()));
         map.addMarker(fromWapointMarker);
         map.addMarker(toWapointMarker);
     }
@@ -85,49 +84,22 @@ public class RouteSegment {
     /**
      * Add a proximity alert at the destination Waypoint
      */
-    private void addProximityAlert(Activity activity) {
-        LocationManager locationManager = (LocationManager) activity.getSystemService(Activity.LOCATION_SERVICE);
+    private void addProximityAlert() {
+        LocationManager locationManager = (LocationManager) getContext().getSystemService(Activity.LOCATION_SERVICE);
         Intent proximityAlert = new Intent();
         proximityAlert.setAction("com.github.weltkulturschnitzelbamberg.weltkulturerbebambergapp.PROXIMITY_ALERT");
-        proximityAlert.putExtra("waypoint-name", toWaypoint.getName());
-        proximityAlert.putExtra("quiz-id", toWaypoint.getQuizID());
+        proximityAlert.putExtra("waypoint-name", route.getWaypointById(toWaypointID).getName());
+        proximityAlert.putExtra("quiz-id", route.getWaypointById(toWaypointID).getQuizId());
 
         int detectionRadius = 40; // The radius around the central point in which to send an proximity alert
         int expirationTime = -1; // The time in milliseconds it takes this proximity alert to expire (-1 = no expiration)
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(activity, 0, proximityAlert, PendingIntent.FLAG_UPDATE_CURRENT); // TODO Previous Proximity Alerts have to be removed
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getContext(), 0, proximityAlert, PendingIntent.FLAG_UPDATE_CURRENT); // TODO Previous Proximity Alerts have to be removed
 
         // TODO Security Error
-        locationManager.addProximityAlert(toWaypoint.getLatitude(), toWaypoint.getLongitude(), detectionRadius, expirationTime, pendingIntent);
+        locationManager.addProximityAlert(route.getWaypointById(toWaypointID).getLatitude(), route.getWaypointById(toWaypointID).getLongitude(), detectionRadius, expirationTime, pendingIntent);
     }
 
-    private void addPolylineToMap(Activity activity, GoogleMap map) {
-        List<LatLng> points = new ArrayList<>();
-
-        try {
-            JSONParser parser = new JSONParser();
-            Object obj = parser.parse(new InputStreamReader(activity.getAssets().open(filename)));
-
-            if (obj instanceof JSONObject) {
-                JSONObject jsonObject = (JSONObject) obj;
-
-                for (String coordinates : ((String)jsonObject.get("polyline")).split(",0.0")) {
-                    String longitude = coordinates.substring(0, coordinates.indexOf(","));
-                    String latitude = coordinates.substring(coordinates.indexOf(",") +1);
-
-                    LatLng point = new LatLng(Float.parseFloat(latitude), Float.parseFloat(longitude));
-                    points.add(point);
-                }
-            }
-        } catch (ParseException | IOException e) {
-            e.printStackTrace();
-        }
-
-        PolylineOptions polyline = new PolylineOptions();
-        polyline.addAll(points);
-        polyline.color(activity.getResources().getColor(R.color.PrimaryColor)); // TODO Deprecated Method
-        polyline.width(25);
-        polyline.geodesic(true);
-
+    private void addPolylineToMap(GoogleMap map) {
         map.addPolyline(polyline);
     }
 }
