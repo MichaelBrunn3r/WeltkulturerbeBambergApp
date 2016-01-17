f6294bde24503dcc8296ff607c8d94f1a7c0933a
OrangeUtan <Oran9eUtan@gmail.com>
2016-01-17 10:47:30 +0100

    ProximityAlertReceiver sendet jetzt eine Notification wenn er einen ProximityAlert empfängt

--- app/src/main/java/com/github/wksb/wkebapp/ProximityAlertReceiver.java
+++ app/src/main/java/com/github/wksb/wkebapp/ProximityAlertReceiver.java
@@ -1,9 +1,12 @@
 package com.github.wksb.wkebapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 
 import com.github.wksb.wkebapp.activity.navigation.NavigationActivity;
@@ -22,10 +25,21 @@ public class ProximityAlertReceiver extends BroadcastReceiver{
     public void onReceive(Context context, Intent intent) {
         boolean entering = intent.getBooleanExtra(LocationManager.KEY_PROXIMITY_ENTERING, false);
         if (entering) {
-            Intent notifyArrivedAtWaypoint = new Intent(NavigationActivity.ACTION_ARRIVED_AT_WAYPOINT);
-            notifyArrivedAtWaypoint.putExtra(NavigationActivity.TAG_QUIZ_ID, intent.getIntExtra(TAG_QUIZ_ID, -1));
-            notifyArrivedAtWaypoint.putExtra(NavigationActivity.TAG_WAYPOINT_NAME, intent.getStringExtra(TAG_WAYPOINT_NAME));
-            LocalBroadcastManager.getInstance(context).sendBroadcast(notifyArrivedAtWaypoint);
+            Intent openNavigationActivity = new Intent(context, NavigationActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, openNavigationActivity, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(String.format(
+                            context.getResources().getString(R.string.notification_arrived_at_waypoint_title)
+                            , intent.getStringExtra(TAG_WAYPOINT_NAME)))
+                    .setContentText(String.format(
+                            context.getResources().getString(R.string.notification_arrived_at_waypoint_detail)
+                            , intent.getStringExtra(TAG_WAYPOINT_NAME)
+                    ))
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+            ((NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE)).notify(0, builder.build());
         }
     }
 }
