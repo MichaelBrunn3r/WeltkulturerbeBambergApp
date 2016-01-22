36105bbc51353da6ac988e134df6fe8f71067e1a
OrangeUtan <Oran9eUtan@gmail.com>
2016-01-21 23:10:39 -0800

    EndActivity hinzugefügt

---
+++ app/src/main/java/com/github/wksb/wkebapp/activity/EndActivity.java
@@ -0,0 +1,20 @@
+package com.github.wksb.wkebapp.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.github.wksb.wkebapp.R;
+
+public class EndActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_end);
+    }
+
+}
